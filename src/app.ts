5cdeed2ba62b3cab8093e44767ec168f9e3cb7c8
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2025-04-14 01:41:00 +0600

    not found route added and middleware functions separated to middlewares folder

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import userRouter from "./app/modules/user/user.route";
 import studentRouter from "./app/modules/student/student.route";
+import globalErrorHanlder from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
 
 const app: Application = express();
 
@@ -17,15 +19,9 @@ app.get("/", (_req: Request, res: Response) => {
   res.send("Hello Dev!");
 });
 
-app.use((err: any, _req: Request, res: Response, _next: NextFunction): any => {
-  const statusCode = 500;
-  const success = false;
-  const message = err.message || "Something went wrong!";
-  return res.status(statusCode).json({
-    success,
-    message,
-    error: err,
-  });
-});
+//global error handler
+app.use(globalErrorHanlder);
+//not found route
+app.use(notFound);
 
 export default app;
---
+++ src/app/middlewares/globalErrorHandler.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+
+const globalErrorHanlder = (
+  err: any,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+): any => {
+  const statusCode = 500;
+  const success = false;
+  const message = err.message || "Something went wrong!";
+
+  res.status(statusCode).json({
+    success,
+    message,
+    error: err,
+  });
+};
+
+export default globalErrorHanlder;
