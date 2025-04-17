00413b76ec4e1fd5bc9a55838d208d35c0d8f230
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2025-04-17 19:45:53 +0600

    (feature)[transtaction added]:mongoose transaction added to create a student and user properly

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import config from "../../config";
 import Semester from "../semester/semester.model";
 import { Student } from "../student/student.model";
@@ -5,6 +6,7 @@ import { TStudent } from "../student/student.type";
 import { generateStudentId } from "./student.utils";
 import User from "./user.model";
 import { TUser } from "./user.type";
+import AppError from "../../utils/AppError";
 
 const createAStudentIntoDB = async (password: string, payload: TStudent) => {
   //create an empty user object
@@ -18,17 +20,34 @@ const createAStudentIntoDB = async (password: string, payload: TStudent) => {
   if (!semester) {
     throw new Error("Semester not found with the given ID.");
   }
-  userData.id = await generateStudentId(semester);
-  //create an user
-  const newUser = await User.create(userData);
-
-  if (Object.keys(newUser).length) {
+  //mongoose transaction
+  const session = await mongoose.startSession(); //create session
+  try {
+    session.startTransaction(); //start transaction
+    userData.id = await generateStudentId(semester);
+    //create an user
+    const newUser = new User(userData);
+    await newUser.save({ session });
+    if (!newUser.id) {
+      throw new AppError(400, "failed to create an user!");
+    }
     //embded user generated id and reference user _id
     payload.id = newUser.id;
     payload.user = newUser._id;
+
     //create a student
-    const newStudent = await Student.create(payload);
+    const newStudent = new Student(payload);
+    await newStudent.save({ session });
+    if (!newStudent.id) {
+      throw new AppError(400, "failed to create a student!");
+    }
+    await session.commitTransaction();
+    await session.endSession();
     return newStudent;
+  } catch (error: any) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new Error(error.message);
   }
 };
 
