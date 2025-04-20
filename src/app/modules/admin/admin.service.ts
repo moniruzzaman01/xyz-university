93c25f969293695c7b42d746312ed00324ace65f
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2025-04-20 16:46:38 +0600

    (bugfix)[delete admin api]: delete admin api updated to delete admin from admin collection and also from user collection

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -1,6 +1,9 @@
+import mongoose from "mongoose";
 import QueryBuilder from "../../builder/QueryBuilder";
 import Admin from "./admin.model";
 import { TAdmin } from "./admin.type";
+import User from "../user/user.model";
+import AppError from "../../utils/AppError";
 
 const getAllAdminFromDB = async (query: Record<string, unknown>) => {
   return await new QueryBuilder(Admin.find(), query)
@@ -17,11 +20,40 @@ const getAnAdminFromDB = async (id: string) => {
 };
 
 const deleteAnAdminFromDB = async (id: string) => {
-  return await Admin.findOneAndUpdate(
-    { id },
-    { isDeleted: true },
-    { new: true }
-  );
+  const session = await mongoose.startSession();
+  try {
+    session.startTransaction();
+
+    const deletedUser = await User.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      { new: true, session }
+    );
+    if (!deletedUser) {
+      throw new AppError(400, `failed to delete the user with id: ${id}`);
+    }
+    const deletedAdmin = await Admin.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      { new: true, session }
+    );
+    if (!deletedAdmin) {
+      throw new AppError(400, `failed to delete the admin with id: ${id}`);
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
+    return deletedAdmin;
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw error;
+  }
+  // return await Admin.findOneAndUpdate(
+  //   { id },
+  //   { isDeleted: true },
+  //   { new: true }
+  // );
 };
 
 const updateAnAdminFromDB = async (id: string, payload: TAdmin) => {
